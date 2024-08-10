25fc013bd9feaa5ab2c040e6d4978bcaff1f5873
hadar <hadar@HADAR>
2024-08-10 17:55:39 +0300

    Add project files.

---
+++ main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_DEPRECATE  
+#define _CRT_NONSTDC_NO_DEPRECATE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+	return 0;
+}
